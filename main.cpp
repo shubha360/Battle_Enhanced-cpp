27b8fb7a9e9e0225dfa7446040daea324aa59876
Raquibul Islam <shubha360@outlook.com>
2023-09-18 16:01:27 +0600

    interface and screenshot updates

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "include/Combat.h"
 
+#include <conio.h>
+
 int main() {
     Combat combat;
     combat.init("files/battle_ground.txt");
     //combat.init("files/test.txt");
-    combat.startBattle(20);
+
+    combat.printBattleGround("Press any key to begin war!");
+
+    int input = _getch(); // wait for user input before begining war
+
+    combat.startBattle(0);
     combat.endBattle();
 
     return 0;
